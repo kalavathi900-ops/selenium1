6d3d7365b28b0e5876a6516191d58dfa842b1b64
kalavathi900-ops <kalavathi900@gmail.com>
2026-04-01 21:08:11 +0530

    Github class

--- src/github/DemoGit.java
+++ src/github/DemoGit.java
@@ -1,14 +1,22 @@
 package github;
 
 import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
 
 public class DemoGit {
+	WebDriver driver;
+	
   @Test
   public void f() {
+	  driver.get("https://www.google.com/");
   }
   @BeforeTest
   public void beforeTest() {
+	  driver = new ChromeDriver();
+	  driver.manage().window().maximize();
+	  
   }
 
 }
